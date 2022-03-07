48c6e8ba8d9968680ec7c07262102615b4224415
tothpeti2000 <60776365+tothpeti2000@users.noreply.github.com>
2022-03-07 13:15:53 +0100

    Product initial schema added

---
+++ Domain/Models/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models
+{
+    internal class Product
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public int Stock { get; set; }
+        
+        public int CategoryID { get; set; }
+        public Category Category { get; set; }
+
+    }
+}
