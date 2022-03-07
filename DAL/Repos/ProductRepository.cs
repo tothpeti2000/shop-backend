7465106b4dba8f22084f9833e3840cc26ac43579
tothpeti2000 <60776365+tothpeti2000@users.noreply.github.com>
2022-03-07 16:35:46 +0100

    GetAllProducts implemented

---
+++ DAL/Repos/ProductRepository.cs
@@ -0,0 +1,33 @@
+﻿using DAL.DbObjects;
+using Domain.Models;
+using Domain.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Repos
+{
+    internal class ProductRepository : IProductRepository
+    {
+        private readonly ProductContext db;
+
+        public ProductRepository(ProductContext db)
+        {
+            this.db = db;
+        }
+
+        public List<Product> GetAllProducts()
+        {
+            return db.Products
+                .Select(ToModel)
+                .ToList();
+        }
+
+        private Product ToModel(DbProduct dbProduct)
+        {
+            return new Product(dbProduct.ID, dbProduct.Name, dbProduct.Price, dbProduct.Stock, dbProduct.Category.Name);
+        }
+    }
+}
