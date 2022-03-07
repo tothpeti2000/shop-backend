5f77639d9f2b068823bb6d4f2b1e977a111d46e9
tothpeti2000 <60776365+tothpeti2000@users.noreply.github.com>
2022-03-07 16:37:44 +0100

    Constructor added to Product

--- Domain/Models/Product.cs
+++ Domain/Models/Product.cs
@@ -8,6 +8,15 @@ namespace Domain.Models
 {
     public class Product
     {
+        public Product(int Id, string name, double price, int stock, string category)
+        {
+            this.ID = Id;
+            this.Name = name;
+            this.Price = price;
+            this.Stock = stock;
+            this.Category = category;
+        }
+
         public int ID { get; set; }
         public string Name { get; set; }
         public double Price { get; set; }
