3cc0dba5014176a9fcf5cf04c9d8ee23f9521cf6
tothpeti2000 <60776365+tothpeti2000@users.noreply.github.com>
2022-03-07 13:51:43 +0100

    ProductContext added

---
+++ DAL/ProductContext.cs
@@ -0,0 +1,17 @@
+﻿using DAL.DbObjects;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    internal class ProductContext: DbContext
+    {
+        public DbSet<DbProduct> Products { get; set; }
+
+        public ProductContext(DbContextOptions options): base(options) { }
+    }
+}
