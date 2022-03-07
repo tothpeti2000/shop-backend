3c96799f6c6bd23022bef1d07ca2fb9df3b74e6e
tothpeti2000 <60776365+tothpeti2000@users.noreply.github.com>
2022-03-07 13:17:14 +0100

    Category initial schema added

--- Domain/Models/Category.cs
+++ Domain/Models/Category.cs
@@ -8,5 +8,10 @@ namespace Domain.Models
 {
     internal class Category
     {
+        public int ID { get; set; }
+        public string Name { get; set; }
+
+        public int ParentCategoryID { get; set; }
+        public Category ParentCategory { get; set; }
     }
 }
