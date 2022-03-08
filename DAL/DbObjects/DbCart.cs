4014b80dfcae8e9e9ce46608d2173425869cbf59
tothpeti2000 <60776365+tothpeti2000@users.noreply.github.com>
2022-03-08 19:29:09 +0100

    Entities added to schema

---
+++ DAL/DbObjects/DbCart.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.DbObjects
+{
+    internal class DbCart
+    {
+        public int ID { get; set; }
+
+        public int CustomerID { get; set; }
+        public AspNetUser Customer { get; set; }
+
+        public List<DbCartItem> Items { get; set; }
+    }
+}
