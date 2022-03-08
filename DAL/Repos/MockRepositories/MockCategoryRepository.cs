cd7d74efdbba0da5d7923a5f7458b2b2a464cbbd
tothpeti2000 <60776365+tothpeti2000@users.noreply.github.com>
2022-03-08 09:39:20 +0100

    MockCategoryRepository added

---
+++ DAL/Repos/MockRepositories/MockCategoryRepository.cs
@@ -0,0 +1,31 @@
+﻿using Domain.Models;
+using Domain.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Repos.MockRepositories
+{
+    public class MockCategoryRepository : ICategoryRepository
+    {
+        private List<Category> categories = new List<Category>()
+        {
+            new Category(1, "Construction toys", null),
+            new Category(2, "LEGO", "Construction toys"),
+            new Category(3, "F1 LEGO", "LEGO"),
+            new Category(4, "Cars", null)
+        };
+
+        public List<Category> GetAllCategories()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Dictionary<Category, List<Category>> GetCategoryHierarchy()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
