3223256b94ecf4233223d60836f0e6d87734dc67
tothpeti2000 <60776365+tothpeti2000@users.noreply.github.com>
2022-03-08 22:32:06 +0100

    DbContext DI added

--- API/Program.cs
+++ API/Program.cs
@@ -1,12 +1,15 @@
+using DAL;
 using DAL.Repos;
 using DAL.Repos.MockRepositories;
 using Domain.Repositories;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<ShopContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("ConnStr")));
 builder.Services.AddCors(c => c.AddPolicy("AllowOrigin", options => options.AllowAnyOrigin()));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
