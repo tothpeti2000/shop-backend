c0cf7cd12f4c961b65d3b9892db534972a12fc64
tothpeti2000 <60776365+tothpeti2000@users.noreply.github.com>
2022-03-08 09:58:13 +0100

    CategoriesController added

---
+++ API/Controllers/CategoriesController.cs
@@ -0,0 +1,30 @@
+﻿using Domain.Models;
+using Domain.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly ICategoryRepository repository;
+
+        public CategoriesController(ICategoryRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        public ActionResult<List<Category>> GetAllCategories()
+        {
+            return repository.GetAllCategories();
+        }
+
+        [Route("hierarchy")]
+        public ActionResult<Dictionary<Category, List<Category>>> GetHierarchy()
+        {
+            return repository.GetCategoryHierarchy();
+        }
+    }
+}
--- API/Program.cs
+++ API/Program.cs
@@ -1,4 +1,5 @@
 using DAL.Repos;
+using DAL.Repos.MockRepositories;
 using Domain.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -12,6 +13,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddScoped<IProductRepository, MockProductRepository>();
+builder.Services.AddScoped<ICategoryRepository, MockCategoryRepository>();
 
 var app = builder.Build();
 
