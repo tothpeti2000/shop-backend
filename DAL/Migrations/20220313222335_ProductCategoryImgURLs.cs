590845cb6ad612aaee5815d88ff89550cba74309
tothpeti2000 <60776365+tothpeti2000@users.noreply.github.com>
2022-03-13 23:33:29 +0100

    Migration for ImgURL added

---
+++ DAL/Migrations/20220313222335_ProductCategoryImgURLs.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DAL.Migrations
+{
+    public partial class ProductCategoryImgURLs : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Product",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ImgURL",
+                table: "Product",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ImgURL",
+                table: "Category",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Product");
+
+            migrationBuilder.DropColumn(
+                name: "ImgURL",
+                table: "Product");
+
+            migrationBuilder.DropColumn(
+                name: "ImgURL",
+                table: "Category");
+        }
+    }
+}
