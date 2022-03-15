28cbe96b06689cee9bc41dcdd41fb5442f119e2f
tothpeti2000 <60776365+tothpeti2000@users.noreply.github.com>
2022-03-15 14:58:45 +0100

    Model Product properties modified

--- Domain/Models/ProductDTOs/Product.cs
+++ Domain/Models/ProductDTOs/Product.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Domain.Models.CategoryDTOs;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,21 +9,26 @@ namespace Domain.Models.ProductDTOs
 {
     public class Product
     {
-        public Product(int Id, string name, double price, int stock, string category)
+        /*public Product(int Id, string name, double price, int stock, string category)
         {
             ID = Id;
             Name = name;
             Price = price;
             Stock = stock;
             Category = category;
-        }
+        }*/
 
         public int ID { get; set; }
         public string Name { get; set; }
         public double Price { get; set; }
         public int Stock { get; set; }
-        public string Category { get; set; }
-        public List<int>? ReviewIDs { get; set; }
+        public string Description { get; set; }
+        public double AverageRating { get; set; }
+        public string? ImgURL { get; set; }
+        public Category Category { get; set; }
+        public List<Review>? Reviews { get; set; }
+
+        public Product() { }
 
     }
 }
