8fd035d155aac7947759eebc19858c831d6d9993
tothpeti2000 <60776365+tothpeti2000@users.noreply.github.com>
2022-03-16 21:45:04 +0100

    ProductService added

---
+++ Domain/Services/ProductService.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Services
+{
+    public class ProductService
+    {
+        private readonly IProductRepository repository;
+
+        public ProductService(IProductRepository repository)
+        {
+            this.repository = repository;
+        }
+    }
+}
