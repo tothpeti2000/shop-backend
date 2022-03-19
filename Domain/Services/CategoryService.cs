bbfaacd2972cbafbb375119b0956542e251e4d7d
tothpeti2000 <60776365+tothpeti2000@users.noreply.github.com>
2022-03-19 17:25:18 +0100

    CategoryService added

---
+++ Domain/Services/CategoryService.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Services
+{
+    public class CategoryService
+    {
+        private readonly ICategoryRepository repository;
+
+        public CategoryService(ICategoryRepository repository)
+        {
+            this.repository = repository;
+        }
+    }
+}
