1c5597015fc8b303e00677fc909648f44e8c7f25
tothpeti2000 <60776365+tothpeti2000@users.noreply.github.com>
2022-03-19 18:03:33 +0100

    Model Category properties modified

--- Domain/Models/Category.cs
+++ Domain/Models/Category.cs
@@ -8,15 +8,9 @@ namespace Domain.Models
 {
     public class Category
     {
-        public Category(int Id, string name, string parentCategoryName)
-        {
-            ID = Id;
-            Name = name;
-            ParentCategoryName = parentCategoryName;
-        }
-
         public int ID { get; set; }
         public string Name { get; set; }
-        public string ParentCategoryName { get; set; }
+        public string? ImgURL { get; set; }
+        public Category? ParentCategory { get; set; }
     }
 }
