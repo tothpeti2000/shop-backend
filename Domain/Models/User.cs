ec92bf8b4575396c4888d1f57d872c4dc6f54446
tothpeti2000 <60776365+tothpeti2000@users.noreply.github.com>
2022-03-20 20:45:26 +0100

    Model User added

---
+++ Domain/Models/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models
+{
+    public class User
+    {
+        [Required]
+        public string UserName { get; set; }
+
+        [Required]
+        [RegularExpression("^[a-zA-Z0-9_\\.-]+@([a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}$", ErrorMessage = "Email is not valid")]
+        public string Email { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+    }
+}
