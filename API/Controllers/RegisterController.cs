bb4c25bbafd911120337ca1770fdf9dcd23978b9
tothpeti2000 <60776365+tothpeti2000@users.noreply.github.com>
2022-03-20 20:51:43 +0100

    RegisterController added

---
+++ API/Controllers/RegisterController.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegisterController : ControllerBase
+    {
+        public ActionResult RegisterUser(User user)
+        {
+
+        }
+    }
+}
