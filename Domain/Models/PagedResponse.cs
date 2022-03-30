8e92799949a0a09e99aeafae16991234c6ba44e6
tothpeti2000 <60776365+tothpeti2000@users.noreply.github.com>
2022-03-30 14:42:45 +0200

    PagedResponse added

---
+++ Domain/Models/PagedResponse.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models
+{
+    public class PagedResponse
+    {
+        public int PageNumber { get; set; }
+        public int PageSize { get; set; }
+        public int TotalPages { get; set; }
+        public Uri NextPage { get; set; }
+        public Uri PreviousPage { get; set; }
+    }
+}
