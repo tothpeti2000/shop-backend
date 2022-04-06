e687741fdee54d2548361ae244488cd0a34f4e29
tothpeti2000 <60776365+tothpeti2000@users.noreply.github.com>
2022-04-06 10:04:11 +0200

    MappingProfile separated into separate mapping Profiles

---
+++ Domain/Mapping/Profiles/CategoryProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using Domain.Models;
+using Domain.Models.CategoryDTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Mapping.Profiles
+{
+    public class CategoryProfile: Profile
+    {
+        public CategoryProfile()
+        {
+            CreateMap<Category, CategoryNode>();
+        }
+    }
+}
---
+++ Domain/Mapping/Profiles/ProductProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using Domain.Models;
+using Domain.Models.ProductDTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Mapping.Profiles
+{
+    public class ProductProfile: Profile
+    {
+        public ProductProfile()
+        {
+            CreateMap<Product, ProductListItem>();
+            CreateMap<Product, ProductDetails>();
+        }
+    }
+}
