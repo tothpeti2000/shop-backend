1c3b12b9d196c5308784a7419e5ead5642ccd461
tothpeti2000 <60776365+tothpeti2000@users.noreply.github.com>
2022-04-19 15:42:08 +0200

    PagedResponse schema modified

--- Domain/Models/PagedResponse.cs
+++ Domain/Models/PagedResponse.cs
@@ -8,13 +8,10 @@ namespace Domain.Models
 {
     public class PagedResponse<T>
     {
-        public List<T> Items { get; set; }
+        public T[] Items { get; set; }
 
         public int CurrentPage { get; set; }
         public int TotalPages { get; set; }
         public int TotalItems { get; set; }
-
-        public Uri nextPage { get; set; }
-        public Uri previousPage { get; set; }
     }
 }
