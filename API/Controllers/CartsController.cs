6644d0852fad852c7a1d8fd8c714cb6ca179e0cd
tothpeti2000 <60776365+tothpeti2000@users.noreply.github.com>
2022-04-21 22:35:46 +0200

    Cart related classes and interfaces added

---
+++ API/Controllers/CartsController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CartsController : ControllerBase
+    {
+        public async Task<ActionResult> AddItemToCart()
+        {
+            return Ok();
+        }
+    }
+}
