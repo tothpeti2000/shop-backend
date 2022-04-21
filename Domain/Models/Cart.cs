8dd6ee15509be81aff6dbc589198adacbc37d2fc
tothpeti2000 <60776365+tothpeti2000@users.noreply.github.com>
2022-04-21 23:34:47 +0200

    Cart and CartItem schemas added

---
+++ Domain/Models/Cart.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models
+{
+    public class Cart
+    {
+        public int ID { get; set; }
+        public User Customer { get; set; }
+        public List<CartItem> Items { get; set; }
+    }
+}
---
+++ Domain/Models/CartItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models
+{
+    public class CartItem
+    {
+        public int ID { get; set; }
+        public int Amount { get; set; }
+        public Product Product { get; set; }
+        public Cart Cart { get; set; }
+    }
+}
