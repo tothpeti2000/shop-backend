87597142057d21d49a2bdd0b72b51df1a48eb516
tothpeti2000 <60776365+tothpeti2000@users.noreply.github.com>
2022-04-22 13:12:44 +0200

    TokenHandler extracted from UserRepository

---
+++ API/TokenHandler.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace API
+{
+    public class TokenHandler
+    {
+        private readonly IConfiguration config;
+
+        public TokenHandler(IConfiguration config)
+        {
+            this.config = config;
+        }
+
+        public string GenerateJWTToken(string userID)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var tokenKey = Encoding.UTF8.GetBytes(config.GetSection("JWT:Key").Value);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[]
+                {
+                    new Claim(ClaimTypes.Name, userID)
+                }),
+                Issuer = config.GetSection("JWT:Issuer").Value,
+                Audience = config.GetSection("JWT:Audience").Value,
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(tokenKey), SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            return tokenHandler.WriteToken(token);
+        }
+    }
+}
--- DAL/Repos/UserRepository.cs
+++ DAL/Repos/UserRepository.cs
@@ -2,14 +2,9 @@
 using Domain.Models;
 using Domain.Repositories;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.Extensions.Configuration;
-using Microsoft.IdentityModel.Tokens;
 using System;
 using System.Collections.Generic;
-using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
-using System.Security.Claims;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace DAL.Repos
@@ -17,12 +12,10 @@ namespace DAL.Repos
     public class UserRepository : IUserRepository
     {
         private readonly UserManager<DbUser> userManager;
-        private readonly IConfiguration config;
 
-        public UserRepository(UserManager<DbUser> userManager, IConfiguration config)
+        public UserRepository(UserManager<DbUser> userManager)
         {
             this.userManager = userManager;
-            this.config = config;
         }
 
         public async Task<AsyncResult> CreateUserAsync(User user)
@@ -71,30 +64,8 @@ namespace DAL.Repos
 
             return new AsyncResult
             {
-                Succeeded = true,
-                Token = GenerateJWTToken(user)
+                Succeeded = true
             };
-        }
-
-        private string GenerateJWTToken(DbUser user)
-        {
-            var tokenHandler = new JwtSecurityTokenHandler();
-            var tokenKey = Encoding.UTF8.GetBytes(config.GetSection("JWT:Key").Value);
-
-            var tokenDescriptor = new SecurityTokenDescriptor
-            {
-                Subject = new ClaimsIdentity(new Claim[]
-                {
-                    new Claim(ClaimTypes.Name, user.Id)
-                }),
-                Issuer = config.GetSection("JWT:Issuer").Value,
-                Audience = config.GetSection("JWT:Audience").Value,
-                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(tokenKey), SecurityAlgorithms.HmacSha256Signature)
-            };
-
-            var token = tokenHandler.CreateToken(tokenDescriptor);
-
-            return tokenHandler.WriteToken(token);
         }
     }
 }
