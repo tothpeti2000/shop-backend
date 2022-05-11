406dee8f71127e91f915087b70e44e75691857c6
tothpeti2000 <60776365+tothpeti2000@users.noreply.github.com>
2022-05-11 12:27:23 +0200

    MappingProfile for Cart added

---
+++ DAL/Profiles/DbCartProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using DAL.DbObjects;
+using Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Profiles
+{
+    public class DbCartProfile: Profile
+    {
+        public DbCartProfile()
+        {
+            CreateMap<DbCartItem, CartItem>();
+        }
+    }
+}
